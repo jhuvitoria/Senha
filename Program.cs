036300190000ce5d801a9306c491a994703d9c78
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-03 21:24:05 -0300

    exercicio concluido

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.ForegroundColor = ConsoleColor.DarkMagenta; 
+Console.Write("ola usuário, digite sua senha:");
+Console.ResetColor();
+
+string senha = Console.ReadLine()!;
+
+const string senhacorreta = "1234abcd"!;
+string permissaoaoacesso = Console.ReadLine()!;
+
+permissaoaoacesso = senha == senhacorreta ? "permitido":"negado";
+
+Console.WriteLine($"acesso {permissaoaoacesso}");
